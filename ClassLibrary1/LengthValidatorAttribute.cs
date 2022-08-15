85ca797f15391570664f522f83dfb3c67710e107
Dilraj M <dilraj.m@philips.com>
2022-08-15 17:30:40 +0530

    Final Assignment

---
+++ ClassLibrary1/LengthValidatorAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectValidationLib
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class LengthValidatorAttribute : ValidationAttribute
+    {
+        public LengthValidatorAttribute()
+        {
+            ValidationName = "Valida Length";
+        }
+        public int MaxLength { get; set; }
+
+        public override bool Validate(object data)
+        {            
+            string datastr = data.ToString();
+            if (datastr.Length > MaxLength)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
